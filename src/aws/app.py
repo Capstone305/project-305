afa9f683f5178eda759cecbbf2c398e7a8060fac
Athena Siaotong <asiaotong@ucmerced.edu>
2023-05-04 14:46:48 -0700

    Worked on routes.

--- src/aws/app.py
+++ src/aws/app.py
@@ -1,11 +1,27 @@
-from flask import Flask
+from flask import *
 app = Flask(__name__)
 
-@app.route('/test')
-def testing():
-    return "Hello World!"
+print("Print statements from Flask should work.")
 
-from routes import *
+@app.route('/test2')
+def testing2():
+    return "Hello World Again!"
+
+@app.route("/submitform", methods=["GET", "POST"])
+def form_response():
+    '''
+    Gets the video uploaded by the user.
+    '''
+
+    import os
+    print(os.getcwd())
+
+    if request.method == 'POST':
+        # User has submitted form
+        print("A post request has been submitted.")
+
+    print("Form response recieved.")
+    return render_template("../../templates/loading.html")
 
 if __name__ == '__main__':
     print("Flask app is up and running.")
