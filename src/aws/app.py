d59d2860126fe90039cf95edcd59a62fa166906e
unknown <asiaotong@ucmerced.edu>
2023-05-11 04:26:13 -0700

    Added Lucas's commits switching the Electron app to a Flask app.

--- src/aws/app.py
+++ src/aws/app.py
@@ -1,11 +1,41 @@
-from flask import Flask
-app = Flask(__name__)
+from flask import Flask, request, render_template
+app = Flask(__name__, template_folder='../templates', static_folder='../static')
 
-@app.route('/test')
-def testing():
-    return "Hello World!"
 
-from routes import *
+@app.route("/")
+def home():
+    '''
+    Displays the homepage.
+    '''
+
+    return render_template("index.html")
+
+
+@app.route("/processing", methods=["GET", "POST"])
+def form_response():
+    '''
+    Gets the video uploaded by the user.
+    '''
+
+    import os
+    print(os.getcwd())
+
+    if request.method == 'POST':
+        # User has submitted form
+        print("A post request has been submitted.")
+
+    print("Form response recieved.")
+    return render_template("loading.html")
+
+
+@app.route("/finished", methods=["GET"])
+def finished():
+    '''
+    Displays the download page.
+    '''
+
+    return render_template("../templates/download.html")
+
 
 if __name__ == '__main__':
     print("Flask app is up and running.")
